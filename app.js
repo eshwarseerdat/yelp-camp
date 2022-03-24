769ab1bcf165e1e11d34096ea33f725ae01421a4
Eshwar Seerdat <eshwarseerdat@gmail.com>
2022-03-24 06:42:54 -0400

    created model file and campground model and required it in app.js, and a test route with fake data

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const path = require("path");
+const campground = require("./model/campground");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error"));
+db.once("open", () => console.log(`Database Connected`));
 
 app = express();
 app.set("view engine", "ejs");
@@ -9,6 +16,16 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/campground", async (req, res) => {
+  const camp = new campground({
+    title: "Backyard",
+    price: "0.01",
+    description: "Camping on a budget",
+  });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen("8080", () => {
