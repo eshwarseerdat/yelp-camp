8e106720f157ae5f30651949ceac918d4045d48d
Eshwar Seerdat <eshwarseerdat@gmail.com>
2022-03-24 06:16:32 -0400

    views folder setup to render ejs file

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const path = require("path");
 
 app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home");
 });
 
 app.listen("8080", () => {
